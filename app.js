bb9a4fd551ca815ca2dff6272943d986c4b04e24
shcces2020 <54838797+shcces2020@users.noreply.github.com>
2020-08-20 17:41:03 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+const video = document.getElementById('video');
+const button = document.getElementById('button');
+const select = document.getElementById('select');
+const canvas = document.getElementById('canvas');
+const photo = document.getElementById('photo');
+
+let currentStream;
+// let width;
+// let height;
+
+function stopMediaTracks(stream) {
+  stream.getTracks().forEach(track => {
+    track.stop();
+  });
+}
+
+function gotDevices(mediaDevices) {
+  select.innerHTML = '';
+  select.appendChild(document.createElement('option'));
+  let count = 1;
+  mediaDevices.forEach(mediaDevice => {
+    if (mediaDevice.kind === 'videoinput') {
+      const option = document.createElement('option');
+      option.value = mediaDevice.deviceId;
+      const label = mediaDevice.label || `Camera ${count++}`;
+      const textNode = document.createTextNode(label);
+      option.appendChild(textNode);
+      select.appendChild(option);
+    }
+  });
+}
+
+button.addEventListener('click', event => {
+  if (typeof currentStream !== 'undefined') {
+    stopMediaTracks(currentStream);
+  }
+  const videoConstraints = {};
+  if (select.value === '') {
+    videoConstraints.facingMode = 'environment';
+  } else {
+    videoConstraints.deviceId = { exact: select.value };
+  }
+  const constraints = {
+    video: videoConstraints,
+    audio: false
+  };
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(stream => {
+      currentStream = stream;
+      video.srcObject = stream;
+      return navigator.mediaDevices.enumerateDevices();
+    })
+    .then(gotDevices)
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+function clearphoto() {
+  var context = canvas.getContext('2d');
+  context.fillStyle = "#AAA";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  var data = canvas.toDataURL('image/png');
+  photo.setAttribute('src', data);
+}
+
+function takepicture() {
+  var context = canvas.getContext('2d');
+  console.log(video.videoWidth);
+
+  if (typeof currentStream !== 'undefined') {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+  
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  } else {
+    clearphoto();
+  }
+}
+
+startbutton.addEventListener('click', function(ev){
+  takepicture();
+  ev.preventDefault();
+}, false);
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices);
+clearphoto()
